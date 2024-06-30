531063d6de0a0bf912fef2731b7bc2edd2ba92de
Yuto Tachibana <tachibanayu24@gmail.com>
2024-06-30 18:52:25 +0900

    Add debug page

---
+++ pages/debug.tsx
@@ -0,0 +1,11 @@
+import { supabase } from "@/utils/supabase";
+import { useEffect, useState } from "react";
+
+// デバッグ用のコードを配置していいページです
+export default function Debug() {
+  return (
+    <div>
+      <h1>This is debug page</h1>
+    </div>
+  );
+}
