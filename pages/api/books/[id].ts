8c38ee9abbe2214d78403bf9b9716249e14a6a09
Yuto Tachibana <tachibanayu24@gmail.com>
2024-07-05 20:53:37 +0900

    編集機能

--- pages/api/books/[id].ts
+++ pages/api/books/[id].ts
@@ -24,6 +24,28 @@ export default async function handler(
     } else {
       res.status(404).json({ message: "Not Found" });
     }
+  } else if (req.method === "PUT") {
+    // req.body.title と req.body.summary とreq.body.comment をそれぞれ変数に入れている
+    // const title = req.body.title;
+    // const summary = req.body.summary; ... と一緒
+    // 分割代入と呼ぶ
+    const { title, summary, comment } = req.body;
+
+    // 実際に発行されるSQL文は以下の通り
+    // UPDATE books
+    //   SET title = title, summary = summary, comment = comment
+    //   WHERE id = req.query.id
+    await supabase
+      .from("books")
+      .update({
+        title,
+        summary,
+        comment,
+      })
+      // eqはequalの略で、update対象のレコードを指定する
+      .eq("id", req.query.id);
+
+    res.status(200).json({ message: "Updated" });
   }
 
   // 未定義のメソッドにリクエストが送信されたとき、405 Method Not Allowedを返す
